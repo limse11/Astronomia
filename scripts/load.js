c3166fcc65c7a2667730010cf647741a16775f33
limse11 <g00glecansuckmypp@gmail.com>
2021-05-19 21:52:27 +0200

    .

--- scripts/load.js
+++ scripts/load.js
@@ -1,4 +1,5 @@
 setTimeout(function load(){
+    document.querySelector('#loading-screen').style.transitionDuration = '1s';
     document.querySelector('#loading-screen').style.height = "0vh";
     document.querySelector('#loading-img').style.display = "none";
 }, 2000);
